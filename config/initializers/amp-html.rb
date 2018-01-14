f692183a0354cd6fb5ce424e8a8acc4fac9f7168
Jonas Hübotter <jonas.huebotter@gmail.com>
2018-01-14 15:56:03 +0100

    install amp-html & mozaic

---
+++ config/initializers/amp-html.rb
@@ -0,0 +1,14 @@
+AmpHtml.configure do |config|
+
+    # Use your regular stylesheets bundled with `app/assets/stylesheets/application.css` in your AMP views. Takes a boolean.
+	# config.inherit_stylesheets = false
+
+
+    ### SplitView
+
+	# Enable SplitView to allow for AMP enabled and AMP disabled versions of your views. Takes a boolean.
+	# config.split_view = false
+	# The default view version if no `amp` param is present in the request. Takes a string.
+	# config.split_view_default = 'amp'
+
+end
